d602e62375a42b970aed666916c029795ee2ce61
Kyrtap24 <patryktupacz@gmail.com>
2024-03-20 14:42:34 +0100

    Finished writing MyNotes app

--- js/main.js
+++ js/main.js
@@ -25,13 +25,67 @@ const closePanel = () => {
 }
 
 const addNote = () => {
-    if (textArea.value !== '') {
-        error.style.visibility = 'hidden'
-    } else {
-        error.style.visibility = 'visible'
-    }
+	if (textArea.value !== '' && category.options[category.selectedIndex].value !== '0') {
+		createNote()
+		error.style.visibility = 'hidden'
+	} else {
+		error.style.visibility = 'visible'
+	}
+}
+
+const createNote = () => {
+	const newNote = document.createElement('div')
+	newNote.classList.add('note')
+	newNote.setAttribute('id', cardID)
+
+	newNote.innerHTML = `
+    <div class="note-header">
+                <h3 class="note-title">${selectedValue}</h3>
+                <button class="delete-note" onclick="deleteNote(${cardID})">
+                    <i class="fas fa-times icon"></i>
+                </button>
+            </div>
+
+            <div class="note-body">
+            ${textArea.value}
+            </div>
+    `
+	checkColor(newNote)
+	noteArea.appendChild(newNote)
+	cardID++
+	textArea.value = ''
+	category.selectedIndex = 0
+	notePanel.style.display = 'none'
+}
+
+const selectValue = () => {
+	selectedValue = category.options[category.selectedIndex].text
+}
+
+const checkColor = note => {
+	switch (selectedValue) {
+		case 'Zakupy':
+			note.style.backgroundColor = 'rgb(72,255,0'
+			break
+		case 'Praca':
+			note.style.backgroundColor = 'rgb(255,243,0'
+			break
+		case 'Inne':
+			note.style.backgroundColor = 'rgb(0,170,255'
+			break
+	}
+}
+
+const deleteNote = id => {
+	const noteToDelete = document.getElementById(id)
+	noteArea.removeChild(noteToDelete)
+}
+
+const deleteAllNotes = () => {
+	noteArea.textContent = ''
 }
 
 addBtn.addEventListener('click', openPanel)
 cancelBtn.addEventListener('click', closePanel)
 saveBtn.addEventListener('click', addNote)
+deleteAllBtn.addEventListener('click', deleteAllNotes)
